e20ce6ccfa9707f8127a01bb719a160b96f8ee61
SebasRomero <sebastian_eromero@hotmail.com>
2025-07-01 19:46:05 -0400

    feat:: add insert fact

--- backend/db.py
+++ backend/db.py
@@ -3,6 +3,8 @@ from import_cat_facts import cur, con
 
 getFactsQuery = "SELECT * FROM cat_facts"
 getRandomFactQuery = "SELECT * FROM cat_facts ORDER BY RANDOM() LIMIT 1"
+insertFactQuery = "INSERT OR IGNORE INTO cat_facts (fact) VALUES (?)"
+getFactByNameQuery = "SELECT * FROM cat_facts WHERE fact=(?)"
 
 def getFacts():
     cur = con.cursor()
@@ -17,5 +19,24 @@ def getRandomFact():
     try:
         res = cur.execute(getRandomFactQuery)
         return res.fetchone()
+    except sqlite3.Error as e:
+        print(e)
+
+def getFactByName(fact: str):
+    cur = con.cursor()
+    try:
+        res = cur.execute(getFactByNameQuery, (fact,))
+        return res.fetchone()
+    except sqlite3.Error as e:
+        print(e)
+
+def insertFact(fact: str):
+    cur = con.cursor()
+    try:
+        cur.execute(insertFactQuery, (fact,))
+        con.commit()
+        return {
+            "Fact": getFactByName(fact)
+        }
     except sqlite3.Error as e:
         print(e)
--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,35 @@
-from fastapi import FastAPI
-import httpx
-from db import getFacts, getRandomFact
+from fastapi import FastAPI, APIRouter, status
+from dto import fact_dto
+import uvicorn
+from db import getFacts, getRandomFact, insertFact
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can limit this to http://localhost:3000 if preferred
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+router = APIRouter()
 
-@app.get("/")
+@router.get("/", status_code=status.HTTP_200_OK, response_description="Server ok!")
 def hello():
     return {"status":"ok"}
 
-@app.get("/catfacts")
+@router.get("/catfacts/", status_code=status.HTTP_200_OK, response_description="All facts!")
 async def getAllFacts():
     return getFacts()
 
-@app.get("/catfacts/random")
+@router.get("/catfacts/random/", status_code=status.HTTP_200_OK, response_description="A random fact!")
 async def getAFact():
     return getRandomFact()
+
+@router.post("/catfacts/", status_code=status.HTTP_201_CREATED, response_description="Fact created!")
+async def createFact(fact: fact_dto.FactDTO):
+    return insertFact(fact=fact.fact)
+
+app.include_router(router)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
